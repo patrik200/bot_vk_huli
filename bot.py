1eb68f07fcde7c84b6da3ee440f056c9ff9fe6d5
patrik200 <68776861+patrik200@users.noreply.github.com>
2022-06-12 22:16:05 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -7,12 +7,14 @@ from vk_api.upload import VkUpload
 from vk_api.utils import get_random_id
 #прописываем токен блять (ссылка на бота https://vk.com/public213872207)
 
+
+photo = ['1.jpg', '2.jpg']
+
 main_token = '446d243249d47a1bfce25de517f658c1cedf58dc210b47ac50004c77c5666a560308926b728424f85bada'
 # прописывем лонг пул (обязательно, связь с вк)
 vk_session = vk_api.VkApi(token = main_token)
 session_api = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
-
 
 
 #функции для отправки фото (начало на 19 строке)
@@ -37,9 +39,15 @@ def send_photo(vk, peer_id, owner_id, photo_id, access_key):
     )
 
 
-def main(session_api):
+def main(session_api, photo):
     upload = VkUpload(session_api)
-    send_photo(session_api, PEER_ID, *upload_photo(upload, '1.jpg'))
+    send_photo(session_api, PEER_ID, *upload_photo(upload, photo))
+
+
+def main_img(session_api, photo):
+    upload = VkUpload(session_api)
+    j = str(photo)
+    send_photo(session_api, PEER_ID, *upload_photo(upload, j))
 
 
 #функции для отправки фото (конец на 43 строчке)
@@ -56,11 +64,17 @@ for event in longpoll.listen(): #чекаем сообщения боту
             msg = event.text.lower() #переводим в нижний регистр сообщения для простого распознования и ифов
             id = event.user_id #id-шник пользователя, ОБЯЗАТЕЛЬНО НАХУЙ
             user = vk_session.method("users.get", {"user_id": id})
-            fullname = user[0]['first_name'] + ' ' + user[0]['last_name']
+            PEER_ID = id
+            fullname = user[0]['first_name']
+            msg_for_1 = None
             for msg_for in msg.split(): #разбиваем строку на массив
-                if msg_for == "пидр":
-                    text = fullname + ' ' + 'пошёл нахуй чмо ебаное'
+                if msg_for == "хуй":
+                    text = fullname + ', ' + 'пошёл нахуй чмо ебаное'
                     sender(id, text) #функции для отправки текстового сообщения
-                    PEER_ID = id
-                    if __name__ == '__main__':
-                        main(session_api) #функции для отправки картинки
+                    main(session_api) #функции для отправки картинки
+                if msg_for_1 == 'fick' and msg_for == 'dich':
+                    text = fullname + ', ' + 'вот ваше заведение!'
+                    sender(id, text)
+                    photo = photo[1]
+                    main_img(session_api, photo)
+                msg_for_1 = msg_for
